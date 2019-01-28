b09edce1ea030c5f72f0a05759c1a50ff5ab601a
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-01-28 10:54:52 -0600

    Completed tables coursework

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  aritst VARCHAR(50) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  release_date INT UNSIGNED NOT NULL,
+  sales FLOAT NOT NULL,
+  genre VARCHAR(50),
+  PRIMARY KEY(id)
+);
