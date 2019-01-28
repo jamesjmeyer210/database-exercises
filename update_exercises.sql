e436ac0f0835be0942713a4c2dff3da69a3f4739
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-01-28 16:37:41 -0600

    Completed statements coureswork.

---
+++ update_exercises.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+
+SELECT name FROM albums;
+SELECT name FROM albums WHERE release_date < 1980;
+SELECT name FROM albums WHERE artist = "Michael Jackson";
+UPDATE albums SET sales = sales * 10;
+UPDATE albums SET release_date = release_date - 100;
+UPDATE albums SET artist = "Peter Jackson" WHERE artist = "Michael Jackson";
